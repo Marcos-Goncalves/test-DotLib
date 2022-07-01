b5c178e570346d275808a2aabbde358158d61a18
Marcos-Goncalves <kirottow17@gmail.com>
2022-07-01 17:27:04 -0300

    feat: validando a requisição para criação do cliente

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Cliente;
+use App\Http\Requests\StoreClienteRequest;
 
 class ClienteController extends Controller
 {
@@ -33,7 +34,7 @@ class ClienteController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreClienteRequest $request)
     {
         $cliente = Cliente::create([
             'nome' => $request->nome,
